7156fa84af388b7f126bb0e597be99d71bfef1d0
leme julian <lemejulian@gmail.com>
2026-04-26 19:04:47 -0300

    Derrota Victoria

---
+++ Assets/Scripts/MenuVictoria.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuVictoria : MonoBehaviour
+{
+    public GameObject defeatMenu; 
+
+    
+    public void ShowDefeatMenu()
+    {
+        defeatMenu.SetActive(true);
+        Time.timeScale = 0f; 
+    }
+
+    
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("Menuinicio"); 
+    }
+
+    
+    public void QuitGame()
+    {
+        Debug.Log("El jugador salió del juego desde el menú de derrota.");
+        Application.Quit();
+    }
+}
